c458e0dbe3698b53b7e4fe58e8ad119758133043
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-01-03 22:45:33 -0500

    added Links to landing page

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Banner from '../../components/Banner';
 import Page from '../../components/Page';
 import List from '../../components/List';
 import ListTitle from '../../components/ListTitle';
 import ListItem from '../../components/ListItem';
+import * as ROUTES from '../../constants/routes';
 
 const Landing = () => {
   return (
@@ -11,7 +13,10 @@ const Landing = () => {
       <Banner />
       <List>
         <ListTitle>How To Play</ListTitle>
-        <ListItem>1) Create an account or log into your account</ListItem>
+        <ListItem>
+          1) <Link to={ROUTES.SIGN_UP}>Create an account</Link> or{' '}
+          <Link to={ROUTES.SIGN_IN}>Log in</Link>
+        </ListItem>
         <ListItem>2) Find a match or host your own</ListItem>
         <ListItem>3) Ready up and wait for your opponent</ListItem>
         <ListItem>4) Face off in an epic duel!</ListItem>
