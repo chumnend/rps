7e2ecf429731083d570a927d867758af72b84fcc
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-01-21 10:52:44 -0500

    moved App from components folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Header from '../Header';
-import Loader from '../Loader';
-import ProtectedRoute from '../ProtectedRoute';
-import Account from '../../containers/Account';
-import Admin from '../../containers/Admin';
-import Game from '../../containers/Game';
-import Games from '../../containers/Games';
-import Home from '../../containers/Home';
-import Landing from '../../containers/Landing';
-import Logout from '../../containers/Logout';
-import NotFound from '../../containers/NotFound';
-import PasswordForget from '../../containers/PasswordForget';
-import SignIn from '../../containers/SignIn';
-import SignUp from '../../containers/SignUp';
-import { useAuth } from '../../store/auth';
-import * as ROUTES from '../../constants/routes';
-import * as Styles from './styles';
+import styled from 'styled-components';
+import Header from './components/Header';
+import Loader from './components/Loader';
+import ProtectedRoute from './components/ProtectedRoute';
+import Account from './containers/Account';
+import Admin from './containers/Admin';
+import Game from './containers/Game';
+import Games from './containers/Games';
+import Home from './containers/Home';
+import Landing from './containers/Landing';
+import Logout from './containers/Logout';
+import NotFound from './containers/NotFound';
+import PasswordForget from './containers/PasswordForget';
+import SignIn from './containers/SignIn';
+import SignUp from './containers/SignUp';
+import * as ROUTES from './constants/routes';
+import { useAuth } from './store/auth';
+import { color } from './themes';
 
 const App = () => {
   const auth = useAuth();
@@ -26,7 +27,7 @@ const App = () => {
   }
 
   return (
-    <Styles.App>
+    <StyledApp>
       <Header auth={auth} />
       <Switch>
         <ProtectedRoute
@@ -95,8 +96,15 @@ const App = () => {
         <Route exact path={ROUTES.LOGOUT} component={Logout} />
         <Route component={NotFound} />
       </Switch>
-    </Styles.App>
+    </StyledApp>
   );
 };
+
+const StyledApp = styled.main`
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  background-color: ${color.lightblue};
+`;
 
 export default App;
