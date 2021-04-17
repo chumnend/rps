7df818f5ceb250ddcdae44edda2010fae7eaf30e
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-04-16 20:02:18 -0400

    moved config for firebase in same folder as firebase context

--- src/store/firebase/firebase.js
+++ src/store/firebase/firebase.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 
-import { auth, db } from '../config';
+import { auth, db } from './config';
 
 const FirebaseContext = React.createContext();
 
