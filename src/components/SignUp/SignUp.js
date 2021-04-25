c91b6b8c6fe3d3a395fb596c2a93d9cbb22225f9
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-04-25 17:32:48 -0400

    firebase refactor 1: registerUser

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 import * as ROUTES from '../../common/constants/routes';
@@ -7,35 +7,20 @@ import Layout from './components/Layout';
 import RegisterForm from './components/RegisterForm';
 
 const SignUp = () => {
-  const [error, setError] = useState(null);
-
   const history = useHistory();
   const firebase = useFirebase();
 
-  const registerUser = (username, email, password) => {
-    firebase
-      .registerUser(email, password)
-      .then((authUser) => {
-        return firebase.getUser(authUser.user.uid).set({
-          id: authUser.user.uid,
-          username,
-          email,
-          wins: 0,
-          losses: 0,
-          admin: false,
-        });
-      })
-      .then(() => {
-        history.push(ROUTES.ACCOUNT);
-      })
-      .catch((error) => {
-        setError(error);
-      });
+  const registerUser = async (username, email, password) => {
+    await firebase.registerUser(username, email, password);
+
+    if (!firebase.user) {
+      history.push(ROUTES.ACCOUNT);
+    }
   };
 
   return (
     <Layout>
-      <RegisterForm registerUser={registerUser} error={error?.message} />
+      <RegisterForm registerUser={registerUser} error={firebase.error} />
     </Layout>
   );
 };
--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -1,30 +1,74 @@
 import PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 
-import { auth } from './config';
-import * as firebaseHelpers from './helpers';
+import { auth, db } from './config';
+
+const initialState = {
+  loading: false,
+  error: false,
+  user: null,
+};
+
+const AUTHENTICATING = 'authenticating';
+const AUTH_SUCCESS = 'auth_succes';
+const AUTH_ERROR = 'auth_error';
+const AUTH_LOGOUT = 'auth_logout';
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case AUTHENTICATING:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case AUTH_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        user: action.user,
+      };
+    case AUTH_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+        user: null,
+      };
+    case AUTH_LOGOUT:
+      return {
+        ...state,
+        loading: false,
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
 
 const FirebaseContext = React.createContext();
 
 const FirebaseProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(async (authUser) => {
-      setLoading(true);
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      dispatch({ type: AUTHENTICATING });
 
-      if (!authUser) {
-        setUser(null);
-        setLoading(false);
+      // check to see if user exists
+      if (!user) {
+        dispatch({ type: AUTH_LOGOUT });
         return;
       }
 
-      const userRef = firebaseHelpers.getUserRef(authUser.uid);
-      const userDoc = await userRef.get();
+      // pull user info from firebase
+      const userRef = db.collection('users').doc(user.uid);
+      const userSnapshot = await userRef.get();
+      const userData = userSnapshot.data();
 
-      setUser(userDoc.data());
-      setLoading(false);
+      // update user state
+      dispatch({ type: AUTH_SUCCESS, user: userData });
     });
 
     return () => {
@@ -32,11 +76,40 @@ const FirebaseProvider = ({ children }) => {
     };
   }, []);
 
-  const firebaseValues = {
-    user,
-    loading,
+  const registerUser = async (username, email, password) => {
+    try {
+      // create user in firebase auth
+      const cred = await auth.createUserWithEmailAndPassword(email, password);
 
-    ...firebaseHelpers,
+      // add user to firestore and append schema
+      const user = cred.user;
+      const userRef = db.collection('users').doc(user.uid);
+
+      await userRef.set({
+        id: user.uid,
+        username,
+        email,
+        wins: 0,
+        losses: 0,
+        admin: false,
+      });
+
+      const userSnapshot = await userRef.get();
+      const userData = userSnapshot.data();
+
+      dispatch({
+        type: AUTH_SUCCESS,
+        user: userData,
+      });
+    } catch (error) {
+      dispatch({ type: AUTH_ERROR, error: error.message });
+    }
+  };
+
+  const firebaseValues = {
+    ...state,
+
+    registerUser,
   };
 
   return (
