fa9a1a0f1d88ba17dbf4b7100b4a5d5b21be5d70
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-04-28 22:14:42 -0400

    added exports to firebase service

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,1 +1,3 @@
+export { auth, db } from './config';
+export { GAMES_COLLECTION_ID, USERS_COLLECTION_ID } from './constants';
 export { FirebaseContext, FirebaseProvider, useFirebase } from './firebase';
