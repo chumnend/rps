1eedb5276159f7986badef134a762d966c9b8024
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-05-08 22:37:43 -0400

    setup UserList and GameList for Admin page

---
+++ src/components/Admin/components/GameList/GameList.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+const GameList = ({ games }) => {
+  return null;
+};
+
+GameList.propTypes = {
+  games: PropTypes.array,
+};
+
+export default GameList;
---
+++ src/components/Admin/components/UserList/UserList.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+const UserList = () => {
+  return null;
+};
+
+UserList.propTypes = {
+  users: PropTypes.array,
+};
+
+export default UserList;
